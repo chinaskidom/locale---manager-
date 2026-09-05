29c13ca298264e216a5888de6fbbf638005f7407
DOMENICO <domenicopace145@gmail.com>
2026-09-05 18:07:27 +0200

    feat: add Cloudflare worker backend

---
+++ worker/index.ts
@@ -0,0 +1,15 @@
+export default {
+  async fetch(request: Request): Promise<Response> {
+    const url = new URL(request.url)
+
+    if (url.pathname === '/api/health') {
+      return Response.json({
+        status: 'ok',
+      })
+    }
+
+    return new Response('Not Found', {
+      status: 404,
+    })
+  },
+}
